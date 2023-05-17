964e4e763de1d0be4ab4133babfcd78cd1c77348
JeonHyoChang <jhc1426@naver.com>
2023-05-17 16:18:44 +0900

    Update deploy.sh

--- BE/scripts/deploy.sh
+++ BE/scripts/deploy.sh
@@ -14,9 +14,12 @@ echo "현재 구동 중인 해플리케이션 pid: $CURRENT_PID"
 if [ -z "$CURRENT_PID" ]; then
   echo "> 현재 구동 중인 애플리케이션이 없으므로 종료하지 않습니다."
 else
-  echo "> kill -15 CURRENT_PID"
-  kill -15 CURRENT_PID
+  echo "> 현재 구동 중인 애플리케이션 종료"
+  kill -15 $CURRENT_PID
   sleep 5
+
+  echo "> 종료 확인 중인 애플리케이션 PID: $CURRENT_PID"
+  sleep 1
 fi
 
 echo "> 새 애플리케이션 배포"
