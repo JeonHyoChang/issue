31acc102f30feb3bc0ccc95127ab7dea37b70693
JeonHyoChang <jhc1426@naver.com>
2023-05-18 00:23:27 +0900

    Update deploy.sh

--- BE/scripts/deploy.sh
+++ BE/scripts/deploy.sh
@@ -7,19 +7,26 @@ echo "> Build 파일복사"
 cp $REPOSITORY/zip/*.jar $REPOSITORY/
 
 echo "> 현재 구동 중인 애플리케이션 pid 확인"
-CURRENT_PID=$(pgrep -fl issue-tracker-0.0.1-SNAPSHOT.jar | grep jar | awk '{print $1}')
-
-echo "현재 구동 중인 해플리케이션 pid: $CURRENT_PID"
-
+#CURRENT_PID=$(pgrep -fl issue-tracker-0.0.1-SNAPSHOT | grep jar | awk '{print $1}')
+#
+#echo "현재 구동 중인 해플리케이션 pid: $CURRENT_PID"
+#if [ -z "$CURRENT_PID" ]; then
+#  echo "> 현재 구동 중인 애플리케이션이 없으므로 종료하지 않습니다."
+#else
+#  echo "> 현재 구동 중인 애플리케이션 종료"
+#  kill -15 $CURRENT_PID
+#  sleep 5
+#
+#  echo "> 종료 확인 중인 애플리케이션 PID: $CURRENT_PID"
+#  sleep 1
+#fi
+CURRENT_PID=$(pgrep -f ${PROJECT_NAME}.*.jar)
 if [ -z "$CURRENT_PID" ]; then
   echo "> 현재 구동 중인 애플리케이션이 없으므로 종료하지 않습니다."
 else
   echo "> 현재 구동 중인 애플리케이션 종료"
   kill -15 $CURRENT_PID
-  sleep 5
-
-  echo "> 종료 확인 중인 애플리케이션 PID: $CURRENT_PID"
-  sleep 1
+  sleep
 fi
 
 echo "> 새 애플리케이션 배포"
