b9f013d391561ac70e1b70bfb1d434b9bfd46106
JeonHyoChang <jhc1426@naver.com>
2023-06-01 16:59:54 +0900

    aaaaaaa

--- fe/src/pages/IssueList.jsx
+++ fe/src/pages/IssueList.jsx
@@ -49,6 +49,7 @@ const IssueList = () => {
   const fetchData = async () => {
     try {
       const url = generateQuery(issueListState);
+      console.log(url)
       const response = await fetch(url);
       const issueListData = await response.json();
       setIssuePageData(issueListData);
